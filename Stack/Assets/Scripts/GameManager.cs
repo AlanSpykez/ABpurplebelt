1a1b46bc6b63e3d5d24d929ad0eac2effb932405
MasterHacker10 <aleks.strzelecki@icloud.com>
2025-03-22 13:44:37 -0400

    db3

---
+++ Stack/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Spawner spawner;
+    // Start is called before the first frame update
+    private void Awake() {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+    void Start()
+    {
+        spawner.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown) {
+            spawner.active = true;
+        }
+    }
+}
